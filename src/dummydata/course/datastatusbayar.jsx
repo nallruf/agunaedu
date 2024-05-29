256c89b25df28a6b56e3221bc5830f7d19de9f34
Bahrul 'Ulum Fadhlur Rohman <bahrululumfr@gmail.com>
2024-05-29 17:09:11 +0700

    update: fix ui status transaction (success, wait, failed)

---
+++ src/dummydata/course/datastatusbayar.jsx
@@ -0,0 +1,48 @@
+import { LuDownload } from "react-icons/lu";
+import { MdOutlineUploadFile, MdReplay } from "react-icons/md";
+import ImgSukses from "../../assets/img/illustration/sukses.png";
+import ImgGagal from "../../assets/img/illustration/gagal.png";
+import ImgWait from "../../assets/img/illustration/wait.png";
+
+const formatDate = (date) => {
+  const options = { day: "numeric", month: "long", year: "numeric" };
+  return new Intl.DateTimeFormat("id-ID", options).format(date);
+};
+
+export const transactionData = {
+  waiting: {
+    imgstatus: ImgWait,
+    title: "Menunggu Pembayaran",
+    message: "Lakukan pembayaran untuk memulai pembelajaran",
+    idPembayaran: "02223977",
+    tanggal: formatDate(new Date()),
+    bayarSebelum: 0.3 * 60,
+    noVirtual: "00036734987",
+    jumlah: "Rp 175.000",
+    buttonText: "Upload Bukti Pembayaran",
+    buttonIcon: <MdOutlineUploadFile />,
+  },
+  success: {
+    imgstatus: ImgSukses,
+    title: "Transaksi Berhasil!",
+    message: "Transaksi mu berhasil, mulai belajar sekarang!",
+    idPembayaran: "02223977",
+    tanggal: formatDate(new Date()),
+    waktu: new Date().toLocaleTimeString(),
+    metodePembayaran: "Gopay",
+    jumlah: "Rp 175.000",
+    buttonText: "Unduh Bukti Transakasi",
+    buttonIcon: <LuDownload />,
+  },
+  failed: {
+    imgstatus: ImgGagal,
+    title: "Transaksi Gagal",
+    message: "Transaksi gagal karena melewati batas waktu pembayaran",
+    idPembayaran: "02223977",
+    tanggal: formatDate(new Date()),
+    metodePembayaran: "Gopay",
+    jumlah: "Rp 175.000",
+    buttonText: "Coba Lagi",
+    buttonIcon: <MdReplay />,
+  },
+};
