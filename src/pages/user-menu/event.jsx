2317d21e7990e83e30094a68498047ba99552d25
yudhaaryo <yudhaaryo1234@gmail.com>
2024-06-03 15:27:52 +0700

    add : dashboard event, challenge, transaction

--- src/pages/user-menu/event.jsx
+++ src/pages/user-menu/event.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import User from "./user/user";
+import { DataEvent } from "../../dummydata/user-menu/dataevent";
+import RenderEvent from "../../components/user-menu/event/renderevent";
 
 const EventUserPage = () => {
   useEffect(() => {
@@ -8,7 +10,19 @@ const EventUserPage = () => {
 
   const content = (
     <>
-      <div className="flex text-primaryBlue font-semibold ">Event</div>
+      <div className="border-2 border-[#EAECF0] rounded-lg py-8 px-2 md:px-10 md:w-3/4 w-full">
+        {DataEvent.map(RenderEvent)}
+        <div className="flex flex-col gap-[6px] mt-4">
+          <h1 className="text-2xl font-semibold text-textPrimary">
+            Akan Datang
+          </h1>
+          <h2 className="text-textPrimary">Event yang akan datang</h2>
+          {DataEvent.map(RenderEvent)}
+          <h1 className="text-2xl font-semibold text-textPrimary">Selesai</h1>
+          <h2 className="text-textPrimary">Event yang sudah selesai</h2>
+          {DataEvent.map(RenderEvent)}
+        </div>
+      </div>
     </>
   );
 
